f0d350945a577dd7c2a9d8f1867c339218d0771b
Vitor Ramos <gramosvitor@gmail.com>
2024-05-16 18:51:19 -0300

    finished project

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -19,25 +19,25 @@ public class Program2 {
 		Department dep = departmentDao.findById(2);
 		System.out.println(dep);
 		
-		System.out.println("==== TEST 2 : findAll ====");
+		System.out.println("\n==== TEST 2 : findAll ====");
 		List<Department> list = departmentDao.findAll();
 		for(Department obj : list) {
 			System.out.println(obj);
 		}
 		
-		/*System.out.println("==== TEST 3 : deleteById ====");
+		System.out.println("==== TEST 3 : deleteById ====");
 		System.out.print("Enter id for delete test: ");
 		int id = sc.nextInt();
 		departmentDao.deleteById(id);
-		System.out.println("Deleted");*/
+		System.out.println("Deleted");
 		
-		System.out.println("==== TEST 4 : update ====");
+		System.out.println("\n==== TEST 4 : update ====");
 		dep = departmentDao.findById(1);
 		dep.setName("Computer");
 		departmentDao.update(dep);
 		System.out.println("Update Completed!");
 		
-		System.out.println("==== TEST 5 : insert ====");
+		System.out.println("\n==== TEST 5 : insert ====");
 		dep = new Department(null, "Makeup");
 		departmentDao.insert(dep);
 		System.out.println("Completed!");
