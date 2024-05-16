fcda2b56b6d8f0e47ced66b5ce89259fe996fce2
Vitor Ramos <gramosvitor@gmail.com>
2024-05-16 18:19:16 -0300

    delete implementation in DepartmentDaoJDBC

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,7 +1,7 @@
 package application;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
@@ -11,6 +11,8 @@ public class Program2 {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
+		
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		
 		System.out.println("==== TEST 1 : findById ====");
@@ -23,7 +25,16 @@ public class Program2 {
 			System.out.println(obj);
 		}
 		
-
+		System.out.println("==== TEST 3 : deleteById ====");
+		System.out.print("Enter id for delete test: ");
+		int id = sc.nextInt();
+		departmentDao.deleteById(id);
+		System.out.println("Deleted");
+		
+		
+		
+		
+		sc.close();
 	}
 
 }
